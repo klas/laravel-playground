35c574b81c9686829b533744b540cd05e95d83d1
Klas Berlic Fras <klas@k-k-k.biz>
2024-06-17 20:52:51 +0200

    revert user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,46 +1,47 @@
 <?php
 
-/**
- * Created by Reliese Model.
- */
-
 namespace App\Models;
 
-use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
 
 /**
- * Class User
- * 
- * @property int $id
- * @property string $name
- * @property string $email
- * @property Carbon|null $email_verified_at
- * @property string $password
- * @property string|null $remember_token
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
+     * The attributes that are mass assignable.
  *
- * @package App\Models
+     * @var array<int, string>
  */
-class User extends Model
-{
-	protected $table = 'users';
-
-	protected $casts = [
-		'email_verified_at' => 'datetime'
-	];
-
-	protected $hidden = [
-		'password',
-		'remember_token'
-	];
-
 	protected $fillable = [
 		'name',
 		'email',
-		'email_verified_at',
 		'password',
-		'remember_token'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
 	];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
