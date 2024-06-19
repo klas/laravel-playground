bae472c994e1b88a5bf08b3b783a1fc7fd72f0a0
Klas Berlic Fras <klas@k-k-k.biz>
2024-06-20 01:32:07 +0200

    cleanup

--- routes/api.php
+++ routes/api.php
@@ -9,14 +9,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-// Experimental
-
-Route::get('whattimeisit', [ExperimentalController::class,'datetime']);
-Route::post('in/', [ExperimentalController::class,'datetime']);
-Route::get('in/', [ExperimentalController::class,'timezone']);
-Route::get('say/{message}', [ExperimentalController::class,'say']);
-
 // Activity
 
 Route::apiResource('activities', ActivityController::class);
